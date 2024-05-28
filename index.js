2c7339cd937d37f7845de6035d8c9a771802b066
JonathanLop1 <147114171+JonathanLop1@users.noreply.github.com>
2024-05-28 01:10:43 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function mostrar(){
+    let clase1 = document.getElementById("regalo111");
+    clase1.classList.add("regalo222");
+}
+function mostrar2(){
+    let clase2 = document.getElementById("regalo22");
+    clase2.classList.add("regalo222");
+}
+function quitar(){
+    let clase1 = document.getElementById("regalo111");
+    clase1.classList.add("regal");
+}
+function quitar2(){
+    let clase1 = document.getElementById("regalo22");
+    clase1.classList.add("regal");
+}
